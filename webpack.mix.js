25dc1707542ed3263f93f44b3745589bb333f206
cbl <lennart.carbe@gmail.com>
2020-08-18 14:07:30 +0200

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,8 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
-mix.js('assets/js/app.js', 'publish/js')
-	.sass('assets/sass/app.scss', 'publish/css')
+mix.js('assets/js/app.js', 'publish/public/js')
+	.sass('assets/sass/app.scss', 'publish/public/css')
 	.options({
 		processCssUrls: false,
 		postCss: [tailwindcss('./tailwind.config.js')],
