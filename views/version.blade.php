9ce5de4e3c2af2ced34f7d676954a242226a5efb
cbl <lennart.carbe@gmail.com>
2020-09-04 11:51:22 +0200

    housekeeping

--- views/version.blade.php
+++ views/version.blade.php
@@ -11,14 +11,6 @@
     </div>
 </div>
 
-<select >
-    <option value="1" selected="selected">
-        1
-    </option>
-    <option value="2">
-        2
-    </option>
-</select>
 <script>
 currentVersion = "{{ $currentVersion }}";
 </script>
